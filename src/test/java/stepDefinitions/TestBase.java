f3125181b5c0fc0d381a0cdeb2d58d2bb7816888
Timofei Stepanov <stepanold76@gmail.com>
2023-09-13 08:55:29 +0200

    Debug version: repair pom repair TestBase and add WebDriverManager make banner escape in Steps add WebDriverManager

--- src/test/java/stepDefinitions/TestBase.java
+++ src/test/java/stepDefinitions/TestBase.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
 
@@ -14,10 +14,17 @@ public class TestBase {
 
     @Before
     public static WebDriver initialize() {
-        System.out.println("Initialize  Web Driver");
-        ChromeOptions ops = new ChromeOptions();
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriverNew.exe");
-        WebDriver driver = new ChromeDriver(ops);
+        //For use driver in src\\main\\resources\\ uncomment next 4 string
+
+//        System.out.println("Initialize  Web Driver");
+//        ChromeOptions ops = new ChromeOptions();
+//        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriverNew.exe");
+//        WebDriver driver = new ChromeDriver(ops);
+
+        // and comment this next 2 string
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return driver;
