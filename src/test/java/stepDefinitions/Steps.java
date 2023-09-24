ba669d3360c09efe8b9ac46615e68478593fe2e6
Roman Burlaka <burlakarv@gmail.com>
2023-09-24 18:28:03 +0200

    stable v.2.1 with smoke e2e verify

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 
 
-public class Steps  {
+public class Steps {
     static WebDriver driver;
     HomePage homePage;
     LoginPage loginPage;
@@ -62,8 +62,7 @@ public class Steps  {
         try {
             //Assertions.assertEquals("Signup / Login", homePage.notLogged());
             System.out.println(driver.getCurrentUrl());
-        }
-        catch (AssertionError e) {
+        } catch (AssertionError e) {
             System.out.println(e.getMessage());
         }
 
@@ -156,6 +155,145 @@ public class Steps  {
     public void robert_account_was_deleted() {
         System.out.println(driver.getCurrentUrl());
     }
+
+    //Scenario: Verify all products and product detail page
+    //*********************************************************************************************************
+    @When("I click to 'Products' button")
+    public void i_click_to_products_button() {
+
+    }
+
+    @And("I verify Product page and product list are visible")
+    public void i_verify_product_page_and_product_list_are_visible() {
+
+    }
+
+    @And("I click view product of first product")
+    public void i_click_view_product_of_first_product() {
+
+    }
+
+    @Then("I am on detail page and verify ':' product name, price, availability, condition, brand")
+    public void i_am_on_detail_page_and_verify_product_name_price_availability_condition_brand() {
+
+    }
+
+    //Scenario: Verify products quantity in cart
+    //*********************************************************************************************************
+
+    @And("I click view product of first")
+    public void i_click_view_product_of_first() {
+
+    }
+
+    @And("I increase quantity to 4")
+    public void i_increase_quantity_to_4() {
+
+    }
+
+    @And("I click 'Add to cart' button")
+    public void i_click_add_to_cart_button() {
+
+    }
+
+    @And("I click 'View cart' button")
+    public void i_click_view_cart_button() {
+
+    }
+    @And("I verify that product is displayed in Cart page")
+    public void i_verify_that_product_is_displayed_in_cart_page() {
+
+    }
+    @Then("I verify  exact quantity in cart")
+    public void i_verify_exact_quantity_in_cart() {
+
+    }
+
+    //Scenario: Verify Test Cases page
+    //*********************************************************************************************************
+    @When("I click on 'Test Cases' button")
+    public void i_click_on_test_cases_button() {
+
+    }
+
+    @Then("I verify that Test Cases page is displayed")
+    public void i_verify_that_test_cases_page_is_displayed() {
+
+    }
+
+    //Scenario: Search product
+    //*********************************************************************************************************
+    @And("I enter product name and click search button")
+    public void i_enter_product_name_and_click_search_button() {
+
+    }
+
+    @And("I verify that searched products are visible")
+    public void i_verify_that_searched_products_are_visible() {
+
+    }
+
+    //Scenario: Add products in cart
+    //*********************************************************************************************************@And("I click to 'Products' button")
+
+
+    @And("I hover over first product and click 'Add to cart'")
+    public void i_hover_over_first_product_and_click_add_to_cart() {
+
+    }
+    @And("I click 'Continue shopping' button")
+    public void i_click_continue_shopping_button() {
+
+    }
+    @And("I hover over second product and click 'Add to cart'")
+    public void i_hover_over_second_product_and_click_add_to_cart() {
+
+    }
+
+    //Scenario: Remove Products from cart
+    //*********************************************************************************************************
+  @And("I click 'X' button for remove product from cart")
+   public void i_click_x_button_for_remove_product_from_cart() {
+
+   }
+    @And("I verify that product removed from cart")
+    public void i_verify_that_product_removed_from_cart() {
+
+    }
+
+    //Scenario: Order Product: Register and Login  before purchase
+    //*********************************************************************************************************
+    @And("I click Proceed to checkout")
+    public void i_click_proceed_to_checkout() {
+
+    }
+    @And("I verify Address Details and Review my Order")
+    public void i_verify_address_details_and_review_my_order() {
+
+    }
+    @And("I enter description in comment text area and click 'Place Order'")
+    public void i_enter_description_in_comment_text_area_and_click_place_order() {
+
+    }
+    @And("I  enter payment details: Name on Card, Card Number, CVC, Expiration date")
+    public void i_enter_payment_details_name_on_card_card_number_cvc_expiration_date() {
+
+    }
+    @And("I  click 'Pay and Confirm Order' button")
+    public void i_click_pay_and_confirm_order_button() {
+
+    }
+    @And("I verify success message 'Your order has been placed successfully!'")
+    public void i_verify_success_message_your_order_has_been_placed_successfully() {
+
+    }
+
+
+
+
+
+
+
 
 
 }
