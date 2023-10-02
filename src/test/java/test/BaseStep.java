5485d383367863ad7af2ad7c66ab797a9b130caf
Timofei Stepanov <stepanold76@gmail.com>
2023-10-02 13:56:51 +0200

    Added @AfterAll to stepDefinitions for close driver

--- src/test/java/test/BaseStep.java
+++ src/test/java/test/BaseStep.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.WebDriver;
 
 public class BaseStep {
 
-    static WebDriver driver = WebDriverSetup.initializeDriver();
-    HomePage homePage = new HomePage(driver);
-    NavigationBar navigationBar = new NavigationBar(driver);
-    LoginPage loginPage = new LoginPage(driver);
-    SignupPage signupPage = new SignupPage(driver);
-    CreateAccountMessage createAccountMessage = new CreateAccountMessage(driver);
-    DeleteAccountMessage deleteAccountMessage = new DeleteAccountMessage(driver);
+        static WebDriver driver = WebDriverSetup.initializeDriver();
 
-
-
-
+        HomePage homePage = new HomePage(driver);
+        NavigationBar navigationBar = new NavigationBar(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SignupPage signupPage = new SignupPage(driver);
+        CreateAccountMessage createAccountMessage = new CreateAccountMessage(driver);
+        DeleteAccountMessage deleteAccountMessage = new DeleteAccountMessage(driver);
+        public static WebDriver getDriver(){
+                return driver;
+        }
 }
