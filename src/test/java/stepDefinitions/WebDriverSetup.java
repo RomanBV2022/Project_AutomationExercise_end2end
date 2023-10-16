3d7ae647f6ddce670c6f1ea827164de9d0edb44a
Timofei Stepanov <stepanold76@gmail.com>
2023-10-16 23:42:27 +0200

    Added implicitlyWait(Duration.ofSeconds(10)); to WebDriverSetup

--- src/test/java/stepDefinitions/WebDriverSetup.java
+++ src/test/java/stepDefinitions/WebDriverSetup.java
@@ -5,12 +5,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class WebDriverSetup {
 
     public static WebDriver turnOnDriver() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
     }
 
