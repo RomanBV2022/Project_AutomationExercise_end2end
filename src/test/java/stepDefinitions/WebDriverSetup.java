8e9af79ec2cda9b7158a034e20f52b31385c4e9d
Timofei Stepanov <stepanold76@gmail.com>
2023-10-16 23:42:27 +0200

    Added implicitlyWait(Duration.ofSeconds(10)); to WebDriverSetup

--- src/test/java/stepDefinitions/WebDriverSetup.java
+++ src/test/java/stepDefinitions/WebDriverSetup.java
@@ -10,39 +10,11 @@ import java.time.Duration;
 public class WebDriverSetup {
 
     public static WebDriver turnOnDriver() {
-
-        String webdriver = System.getProperty("browser","chrome");
-        switch(webdriver) {
-            case "firefox" -> {
-                WebDriverManager.firefoxdriver().setup();
-                WebDriver driver = new FirefoxDriver();
-                driver.manage().window().maximize();
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                return driver;
-            }
-            case "chrome" -> {
-                WebDriverManager.chromedriver().setup();
-                WebDriver driver = new ChromeDriver();
-                driver.manage().window().maximize();
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                return driver;
-            }
-            case "edge" -> {
-                WebDriverManager.edgedriver().setup();
-                WebDriver driver = new EdgeDriver();
-                driver.manage().window().maximize();
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                return driver;
-            }
-//            case "safari" -> {
-//                WebDriverManager.safaridriver().setup();
-//                driver = new SafariDriver();
-//                driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//                return driver;
-//            }
-            default -> throw new RuntimeException("Unsupported webdriver: " + webdriver);
-        }
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
     }
 
 }
